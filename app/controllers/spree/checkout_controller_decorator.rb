5f1d54efe0ccf972d2e1403a42b7f98a8e5748fc
Jerry Walsh <jerry.walsh@gmail.com>
2014-03-05 22:50:41 +1300

    add analytics utm_nooverride=1 to paypal return urls

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -281,8 +281,8 @@ module Spree
         credits_total = credits.map {|i| i[:amount] * i[:quantity] }.sum
       end
 
-      opts = { :return_url        => paypal_confirm_order_checkout_url(order, :payment_method_id => payment_method),
-               :cancel_return_url => edit_order_checkout_url(order, :state => :payment),
+      opts = { :return_url        => paypal_confirm_order_checkout_url(order, :payment_method_id => payment_method, :utm_nooverride => 1),
+               :cancel_return_url => edit_order_checkout_url(order, :state => :payment, :utm_nooverride => 1),
                :order_id          => order.number,
                :custom            => order.number,
                :items             => items,
